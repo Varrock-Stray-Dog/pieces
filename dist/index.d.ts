9c1bab4ee0d4b8ac4919d4f6cce678b45f5bc80f
kyranet <contact@skyra.pw>
2020-08-20 18:59:31 +0000

    build: tsc build for 4cba11777b992c7c63eaa39fc6278cf97104b9d9

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare const main: () => string;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+exports.main = () => 'this builds and pushes';
+//# sourceMappingURL=index.js.map
